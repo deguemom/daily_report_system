6b41b6ff6b1bb0b8f1e9b06d77d5babe6ba6d807
shota.ideguchi <umbrovolante@gmail.com>
2019-10-15 22:10:41 +0900

    Add login / logout

--- src/controllers/login/LoginServlet.java
+++ src/controllers/login/LoginServlet.java
@@ -49,8 +49,9 @@ public class LoginServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
+    // ログイン処理を実行
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     // 認証結果を格納する変数
+        // 認証結果を格納する変数
         Boolean check_result = false;
 
         String code = request.getParameter("code");
